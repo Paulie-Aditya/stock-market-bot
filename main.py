35195be6a1cd36aa37660d7b80916b7c394a587f
Paulie-Aditya <paul.aditya304@gmail.com>
2023-07-06 01:03:02 +0530

    last commit before sleeping (maybe)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import config
 import requests
 
-url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey={config.api_key}'
+keywords = "tesco"
+url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={keywords}&apikey={config.api_key}'
 r = requests.get(url)
 data = r.json()
 
---
+++ mainjs.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    let x = document.getElementById("search").value;
+    let text;
+    
+    /*
+    if (isNaN(x) || x < 1 || x > 10) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+    */
+  }
